b5baa605fe48d3991e9b166c3b23bbec48f15bf0
ahmedsabry23 <ahmed154061@bue.edu.eg>
2023-07-04 12:52:31 -0700

    a

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,2 +1,19 @@
-#include<stdio.h>
+#include "main.h"
 
+/**
+ * main - entry point
+ * Description: prints _putchar using putchar prototype
+ * Return: always 0 (success)
+*/
+int main(void)
+{
+	char str[] = "_putchar";
+	int c;
+
+	for (c = 0; c < 8; c++)
+		_putchar(str[c]);
+	_putchar('\n');
+
+	return (0);
+
+}
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line
+ * @str: string to print
+ */
+void print_rev(char *s)
+{
+	char *str;
+
+	str = "Hello";
+	print_rev(str);
+}
