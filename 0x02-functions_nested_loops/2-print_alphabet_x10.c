c39c7abe0e85a9b8a5d3c691881e412104c82bef
ahmedsabry23 <ahmed154061@bue.edu.eg>
2023-07-04 13:46:19 -0700

    a

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,10 +8,9 @@ void print_alphabet_x10(void)
 	int line, cha;
 
 	for (line = 0; line <= 9; line++)
-
 	{
 		for (cha = 'a'; cha <= 'z'; cha++)
 			_putchar(cha);
-		_putchar('\n')
+		_putchar('\n');
 	}
 }
